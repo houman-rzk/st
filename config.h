9c7943fda7c012d3c8b9f91875fc028f19b8571f
ache <ache@ache.com>
2022-06-08 00:57:18 +0200

    Applied 'copyurl' patch

--- config.h
+++ config.h
@@ -315,6 +315,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ControlMask|ShiftMask,XK_I,           copyurl,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 	{ ShiftMask,            XK_Up,		kscrollup,      {.i = -1} },

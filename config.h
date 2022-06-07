d999fe4168ecb2443521dcec1410f35d33fe997b
ache <ache@ache.com>
2022-06-07 09:01:09 +0200

    Applied 'boxdraw' patch

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Mono:pixelsize=20:antialias=true:autohint=true";
-static int borderpx = 2;
+static char *font = "Mono:pixelsize=15:antialias=true:autohint=true";
+static int borderpx = 6;
 
 /*
  * Override/adjust fontsize of choosen monitors:
@@ -80,6 +80,18 @@ static unsigned int blinktimeout = 800;
  * thickness of underline and bar cursors
  */
 static unsigned int cursorthickness = 2;
+
+/*
+ * 1: render most of the lines/blocks characters without using the font for
+ *    perfect alignment between cells (U2500 - U259F except dashes/diagonals).
+ *    Bold affects lines thickness if boxdraw_bold is not 0. Italic is ignored.
+ * 0: disable (render all U25XX glyphs normally from the font).
+ */
+const int boxdraw = 1;
+const int boxdraw_bold = 0;
+
+/* braille (U28XX):  1: render as adjacent "pixels",  0: use font */
+const int boxdraw_braille = 0;
 
 /*
  * bell volume. It must be a value between -100 and 100. Use 0 for disabling

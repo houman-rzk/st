dcdb97e8ce9b3ea800bac0342e659c3b83c4a604
ache <ache@ache.com>
2022-06-10 23:32:38 +0200

    Applied 'font2' patch

--- config.h
+++ config.h
@@ -6,6 +6,13 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Mono:pixelsize=15:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"NotoColorEmoji:pixelsize=15:antialias=true:autohint=true"
+/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+};
+
 static int borderpx = 6;
 
 /*

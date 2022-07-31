b29e06201ab55da2eee70bac7f367164b392b215
ache <null>
2022-07-31 17:44:28 +0200

    Minor font fix

--- config.h
+++ config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Mono:pixelsize=15:antialias=true:autohint=true";
+//static char *font = "Mono:pixelsize=15:antialias=true:autohint=true";
+static char *font = "monospace:size=11";
 /* Spare fonts */
 static char *font2[] = {
 	"JoyPixels:pixelsize=15:antialias=true:autohint=true"
